6894a5cb2331af547111331c1ac82320a4303004
gyqls980 <hyobin0715@kookmin.ac.kr>
2021-03-27 03:15:51 +0900

    user 기본 구현

---
+++ src/main/java/com/example/security/User.java
@@ -0,0 +1,26 @@
+package com.example.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 300, nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/example/security/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// Spring Security를 사용하기 위해서는 Spring Security Filter Chain 을 사용해야 함
+// -> WebSecurityConfigurerAdapter를 상속받은 클래스에 @EnableWebSecurity 사용
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll();
+    }
+}
